9f7f85687d1ada8eabddade8cba45ebf3ab7ca3a
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-10-05 10:10:57 -0300

    Optimización descripción step

--- src/components/Form/Teacher/Steps/DescriptionStep.jsx
+++ src/components/Form/Teacher/Steps/DescriptionStep.jsx
@@ -4,22 +4,56 @@ import { Form, Input, Button, Typography, Alert } from "antd";
 const { TextArea } = Input;
 const { Title, Paragraph, Link } = Typography;
 
+const stepsContent = [
+  {
+    title: "1. Introduce yourself",
+    description:
+      "Show potential students who you are! Share your teaching experience and passion for education and briefly mention your interests and hobbies.",
+    placeholder: "Hello, my name is... and I'm from...",
+    name: "introduction",
+    message: "Please introduce yourself",
+  },
+  {
+    title: "2. Teaching experience",
+    description: "Share your teaching experience and highlight your skills.",
+    placeholder: "Share your teaching experience...",
+    name: "experience",
+    message: "Please share your experience",
+  },
+  {
+    title: "3. Motivate potential students",
+    description: "Motivate your students and show them why they should choose you as a teacher.",
+    placeholder: "Motivate your students...",
+    name: "motivateStudents",
+    message: "Please motivate your students",
+  },
+  {
+    title: "4. Write a catchy headline",
+    description: "",
+    placeholder: "Write a catchy headline...",
+    name: "headline",
+    message: "Please write a headline",
+  },
+];
+
 const ProfileDescriptionStep = () => {
   const [form] = Form.useForm();
   const [charCount, setCharCount] = useState(0);
-  const [step, setStep] = useState(1); // Controla el estado de cada paso
+  const [step, setStep] = useState(0); // El estado empieza en 0 (primer paso)
 
   const handleTextChange = (e) => {
     setCharCount(e.target.value.length);
   };
 
-  // Función para manejar el cambio de paso
   const handleNextStep = () => {
-    setStep(step + 1);
+    if (step < stepsContent.length - 1) setStep(step + 1);
   };
+
   const handleBackStep = () => {
-    setStep(step - 1);
-  }; 
+    if (step > 0) setStep(step - 1);
+  };
+
+  const { title, description, placeholder, name, message } = stepsContent[step];
 
   return (
     <div className="mx-auto max-w-lg p-6">
@@ -33,127 +67,44 @@ const ProfileDescriptionStep = () => {
       </Paragraph>
 
       <Form form={form} layout="vertical">
-        {/* Step 1: Introduce yourself */}
-        <Title level={3}>1. Introduce yourself</Title>
-        {step == 1 && (
-          <>
-            <Paragraph>
-              Show potential students who you are! Share your teaching
-              experience and passion for education and briefly mention your
-              interests and hobbies.
-            </Paragraph>
-            <Form.Item
-              name="introduction"
-              rules={[{ required: true, message: "Please introduce yourself" }]}
-            >
-              <TextArea
-                rows={6}
-                placeholder="Hello, my name is... and I'm from..."
-                onChange={handleTextChange}
-                maxLength={400}
-              />
-            </Form.Item>
-            <div className="mt-6 flex items-center justify-between">
-              <span>{charCount} / 400</span>
-            </div>
-      
-            
-            <Button className="mb-6" onClick={handleNextStep}>
-              Continue
-            </Button>
-          </>
-        )}
+        <Title level={3}>{title}</Title>
+        {description && <Paragraph>{description}</Paragraph>}
+        <Form.Item name={name} rules={[{ required: true, message }]}>
+          <TextArea
+            rows={6}
+            placeholder={placeholder}
+            onChange={handleTextChange}
+            maxLength={400}
+          />
+        </Form.Item>
 
-        {/* Step 2: Teaching experience */}
-        <Title level={3}>2. Teaching experience</Title>
-        {step == 2 && (
-          <>
-            <Form.Item
-              name="experience"
-              rules={[
-                { required: true, message: "Please share your experience" },
-              ]}
-            >
-              <TextArea
-                rows={6}
-                placeholder="Share your teaching experience..."
-                onChange={handleTextChange}
-                maxLength={400}
-              />
-            </Form.Item>
-            <div className="mt-6 flex items-center justify-between">
-              <span>{charCount} / 400</span>
-            </div>
-            {/* handleBackStep */}
-            <Button className="mb-6 mr-2" onClick={handleBackStep}>
-            Previous
-            </Button>
-            <Button className="mb-6" onClick={handleNextStep}>
-              Continue
-            </Button>
-          </>
-        )}
+        <div className="mt-6 flex items-center justify-between">
+          <span>{charCount} / 400</span>
+        </div>
 
-        {/* Step 3: Motivate potential students */}
-        <Title level={3}>3. Motivate potential students</Title>
-        {step == 3 && (
-          <>
-            <Form.Item
-              name="motivateStudents"
-              rules={[
-                { required: true, message: "Please motivate your students" },
-              ]}
-            >
-              <TextArea
-                rows={6}
-                placeholder="Motivate your students..."
-                onChange={handleTextChange}
-                maxLength={400}
-              />
-            </Form.Item>
-            <div className="mt-6 flex items-center justify-between">
-              <span>{charCount} / 400</span>
-            </div>
-            <Button className="mb-6 mr-2" onClick={handleBackStep}>
-            Previous
-            </Button>
-            <Button className="mb-6" onClick={handleNextStep}>
-              Continue
-            </Button>
-          </>
-        )}
-
-        <Title level={3}>4. Write a catchy headline</Title>
-        {step == 4 && (
-          <>
-            <Form.Item
-              name="headline"
-              rules={[{ required: true, message: "Please write a headline" }]}
-            >
-              <TextArea
-                rows={6}
-                placeholder="Write a catchy headline..."
-                onChange={handleTextChange}
-                maxLength={400}
-              />
-            </Form.Item>
-            <div className="mt-6 flex items-center justify-between">
-              <span>{charCount} / 400</span>
-            </div>
-            <Button className="mb-6" onClick={handleBackStep}>
+        <div className="mt-4">
+          {step > 0 && (
+            <Button onClick={handleBackStep} className="mr-2">
               Previous
             </Button>
-          </>
-        )}
+          )}
+          {step < stepsContent.length - 1 && (
+            <Button type="primary" onClick={handleNextStep}>
+              Continue
+            </Button>
+          )}
+        </div>
       </Form>
+
       <Alert
-              message="Don't include your last name or present your information in a CV format"
-              type="info"
-              showIcon
-              className="mb-4"
-            />
+        message="Don't include your last name or present your information in a CV format"
+        type="info"
+        showIcon
+        className="mt-6"
+      />
     </div>
   );
 };
 
 export default ProfileDescriptionStep;
+
