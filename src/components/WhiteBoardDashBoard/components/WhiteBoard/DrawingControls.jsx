f4998065eb6b761435d267e6b152853217ea4ed2
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-01 19:33:25 -0300

    Empezando paleta de colores

--- src/components/WhiteBoardDashBoard/components/WhiteBoard/DrawingControls.jsx
+++ src/components/WhiteBoardDashBoard/components/WhiteBoard/DrawingControls.jsx
@@ -1,27 +1,13 @@
+import { useState } from "react";
 import { FaPen, FaEraser, FaCircle  } from "react-icons/fa";
 import { FloatButton } from 'antd';
 
 function DrawingControls({ drawingMode, toggleDrawingMode, changeColor }) {
-  return (
-    <>
-    <FloatButton
-        style={{ bottom: 150, right:170}}
-        icon={<FaCircle/> }
-       />
-     <FloatButton
-        style={{ bottom: 150, right:120}}
-        icon={<FaCircle/> }
-       />
-        <FloatButton
-        style={{ bottom: 150, right:70}}
-        icon={<FaCircle/> }
-       />
 
-       {/* quiero que este icono */}
-       <FloatButton
-        style={{ bottom: 150}}
-        icon={<FaCircle/> }
-       />
+
+  return (
+    <> 
+   <ColorOption/>
       <FloatButton
         onClick={toggleDrawingMode}
         type={drawingMode === 'draw' ? "primary" : "danger"}
@@ -33,3 +19,34 @@ function DrawingControls({ drawingMode, toggleDrawingMode, changeColor }) {
 }
 
 export default DrawingControls;
+
+
+const ColorOption = () => {
+    const [paletColorOpened, setPaletColorOpened ] = useState(false)
+
+    return(
+<>
+      {paletColorOpened && (
+        <>
+        <FloatButton
+              style={{ bottom: 150, right:170}}
+              icon={<FaCircle/> }
+             />
+           <FloatButton
+              style={{ bottom: 150, right:120}}
+              icon={<FaCircle/> }
+             />
+              <FloatButton
+              style={{ bottom: 150, right:70}}
+              icon={<FaCircle/> }
+             /></>
+       )}
+             {/* quiero que este icono */}
+             <FloatButton
+              onClick={()=>{setPaletColorOpened(!paletColorOpened) }}
+              style={{ bottom: 150}}
+              icon={<FaCircle/> }
+             />
+</>
+    );
+}
