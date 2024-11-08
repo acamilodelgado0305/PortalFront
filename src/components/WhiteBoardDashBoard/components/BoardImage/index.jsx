58ba1d4327dfde1d73642fc7e1b349ef13608195
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-08 18:59:38 -0300

    Save

--- src/components/WhiteBoardDashBoard/components/BoardImage/index.jsx
+++ src/components/WhiteBoardDashBoard/components/BoardImage/index.jsx
@@ -1,25 +1,15 @@
-import React, { useState, useEffect } from "react";
+import  { useState } from "react";
 import { Rnd } from "react-rnd";
-import { FullscreenOutlined, CloseOutlined } from "@ant-design/icons";
-import { events } from "../../../../enums/whiteboardEvents";
+import {  CloseOutlined } from "@ant-design/icons";
 
 function BoardImage({ url, room, onClose, socket, context }) {
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  useEffect(() => {
-    if(!socket)return 
-    socket.on(events.MOVE_IMAGE, (data) => {
-      console.log(JSON.stringify(data))
-      console.log('Data position '+data.position)
-     setPosition(data.position);
-    });
- 
-  }, [socket]); 
+
 
   const handleDragStop = (e, d) => {
-    console.log('moviendo pero no soy socket   y:'+d.y + 'x:'+d.x)
+    console.log('moviendo pero no soy socket   y:'+d.y + 'x:'+d.x);
     setPosition({ x: d.x, y: d.y });
-    if(!socket) return
-      socket.emit(events.MOVE_IMAGE, { room, position: { x: d.x, y: d.y } });
+    
   };
 
   return (
