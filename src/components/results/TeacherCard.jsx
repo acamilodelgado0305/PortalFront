5ae64ac67d92ab3813036ba6f2cb38fe8f5fac87
Jhon Anderson Garcia <garciacalvojhonanderson@gmail.com>
2024-11-22 19:42:21 -0500

    correccion zona horaria y responsive de del profesor

--- src/components/results/TeacherCard.jsx
+++ src/components/results/TeacherCard.jsx
@@ -12,6 +12,7 @@ import IconoMensaje from '../../assets/icons/send.svg';
 const TeacherCard = ({ teacher, onVideoClick, setShowCalendarModal, setSelectedTeacher }) => {
   const [showScheduleModal, setShowScheduleModal] = useState(false);
   const [showRegisterModal, setShowRegisterModal] = useState(false);
+  const [verMas, setVerMas] = useState(false);
   const [showSendMessageModal, setShowSendMessageModal] = useState(false);
   const { idToken, user } = useAuth();
 
@@ -31,25 +32,26 @@ const TeacherCard = ({ teacher, onVideoClick, setShowCalendarModal, setSelectedT
   };
 
   return (
-    <div className="group relative">
-      <div className="h-[auto] w-[50%]  border border-gray-300 bg-white p-6 shadow-sm transition-all duration-200 hover:border-purple-600 border-2 hover:shadow-md lg:h-[37vh] lg:w-[100%]">
-        <div className="flex gap-6">
+    <div className="group relative md:w-9/12 ">
+      <div className=" md:w-[50%]  border border-gray-300 bg-white p-6 shadow-sm transition-all duration-200 hover:border-purple-600 border-2 hover:shadow-md  lg:w-[100%]">
+        <div className="flex gap-6 flex-col md:flex-row ">
           <div>
             <img
               src={teacher.profileImageUrl}
               alt={`${teacher.firstName} ${teacher.lastName}`}
-              className="h-30 w-30 rounded-lg object-cover lg:h-ful lg:w-[11em] lg:h-[11em]"
+              className="h-30 md:w-30 w-48 rounded-lg object-cover lg:h-ful lg:w-[11em] lg:h-[11em]"
               onError={(e) => {
                 e.target.src = "https://via.placeholder.com/128x128";
               }}
             />
           </div>
 
-          <div className="w-[50%] flex-1 lg:w-[100%]">
-            <div className="flex items-start justify-between">
-              <div className="w-[34]">
+          <div className="md:w-[50%] w-[100%]  flex-1 lg:w-[100%]">
+            <div className="flex items-start justify-between  w-full">
+
+              <div className={verMas?"w-full":`w-full h-[37vh] overflow-hidden`}>
                 <div className="mb-2 flex items-center gap-2">
-                  <h3 className="text-3xl font-medium text-bold">
+                  <h3 className="md:text-3xl text-2xl font-medium text-bold">
                     {teacher.firstName} {teacher.lastName}
                   </h3>
                   <div className="flex items-center">
@@ -67,15 +69,16 @@ const TeacherCard = ({ teacher, onVideoClick, setShowCalendarModal, setSelectedT
                 </div>
 
 
-                <p className="mb-1 text-2xl text-black pb-4 lg:pb-0">
+                <p className="mb-1 text-2xl text-black pb-4 lg:pb-0 ">
                   Enseña: {teacher.subjectYouTeach}
 
                 </p>
-                <div className="w-[34em]">
-                  <p className="mb-1 text-2xl text-black pb-4 lg:pb-0 ">
+                <div className="md:w-[30em] w-full">
+                  <p className="mb-1 md:text-2xl text-base text-black pb-4 lg:pb-0">
 
                     {teacher.description?.introduction}
                   </p>
+                    
 
                 </div>
 
@@ -98,13 +101,16 @@ const TeacherCard = ({ teacher, onVideoClick, setShowCalendarModal, setSelectedT
 
               <div className="lg:text-right">
                 <p className="font-semibold text-3xl">
-                  US {formatPrice(teacher.hourlyRate)}
+                  <p>US</p>
+                  <p className="">{formatPrice(teacher.hourlyRate)}</p>
+                 
                 </p>
-
               </div>
             </div>
 
-            <div className="flex w-[100%] flex-col gap-3 text-right lg:items-end lg:justify-end mt-[-6em]">
+            <p onClick={() => setVerMas(!verMas)} className="underline cursor-pointer hover:text-blue-300 md:mb-0 mb-32">{!verMas?"ver mas":"ver menos"}</p>
+
+            <div className="flex w-[100%] md:flex-col gap-3 text-right lg:items-end lg:justify-end mt-[-6em]">
               <button
                 onClick={() => { !idToken ? setShowRegisterModal(true) : setShowCalendarModal(true), setSelectedTeacher(teacher) }}
                 className="w-[12em] bg-purple-500 text-white py-2 px-4 rounded-lg hover:bg-purple-600 transition-colors duration-200 font-medium"
@@ -129,8 +135,8 @@ const TeacherCard = ({ teacher, onVideoClick, setShowCalendarModal, setSelectedT
         </div>
       </div>
 
-      <div className="absolute left-[30%] top-0 z-10 ml-4 opacity-0 transition-opacity duration-200 group-hover:opacity-100 lg:left-full">
-        <div className="h-[21.8em] w-[27em]  border !border-purple-600 border-2 border-black bg-white p-3 shadow-lg ">
+      <div className="absolute left-[30%] md:top-0 top-[-3em] z-10 ml-4 opacity-0 transition-opacity duration-200 group-hover:opacity-100 lg:left-full">
+        <div className="md:h-[21.8em] md:w-[27em] h-[15em] w-[15em]  border !border-purple-600 border-2 border-black bg-white p-3 shadow-lg ">
           <div
             className={`relative h-[15em] w-full rounded-lg bg-gray-100 ${teacher.video ? "cursor-pointer" : ""} overflow-hidden`}
             onClick={() => teacher.video && onVideoClick(teacher.video)}
