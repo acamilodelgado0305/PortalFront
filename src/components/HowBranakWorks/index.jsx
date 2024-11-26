72c25920e07a9687fc4e261c225eebeb0402094e
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-11-26 12:55:50 -0300

    Modificacion de estilos

--- src/components/HowBranakWorks/index.jsx
+++ src/components/HowBranakWorks/index.jsx
@@ -2,12 +2,12 @@ import Target from "./Target";
 const HowBranakWorks = () => {
   return (
     <div className="bg-white w-full flex flex-col items-center lg:pb-8">
-      <div className=" justify-center w-full max-w-screen-4xl px-6">
-
-        <div className="mb-5  px-4 w-full">
-          <h2 className="text-5xl lg:text-6xl font-bold text-[#86efac] text-left md:pl-20">How Branak works:</h2>
+      <div className=" justify-center w-full md:w-[80%] h-auto m-auto">
+   <div className="mb-5 w-[100%]">
+          <h2 className="text-5xl lg:text-6xl font-bold text-[#86efac] text-left ">How Branak works:</h2>
         </div>
-        <div className="mb-14 flex flex-col lg:flex-row gap-10  w-full sm:px-10 sm:pb-7 md:px-20 ">
+       
+        <div className="mb-14 flex flex-col lg:flex-row gap-10 "> 
           <Target number={1} />
           <Target number={2}  />
           <Target number={3}  />
