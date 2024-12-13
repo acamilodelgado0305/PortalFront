da2dfb000555f51709e78bfd4e63ab87099f8795
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-12-13 16:02:26 -0300

    Guardado

--- src/components/dashboard/student/components/AllClasses.jsx
+++ src/components/dashboard/student/components/AllClasses.jsx
@@ -1,8 +1,10 @@
 import ClassCard from "./ClassCard";
+import { Button, List, Tag } from 'antd';
+import {  UserSearch } from 'lucide-react';
 
 const AllClasses = ({ allClasses, showAllOldClasses, setShowAllOldClasses }) => {
   return (
-    <div className="rounded-lg bg-[#fff] p-6 shadow-lg">
+    <div className={`rounded-lg bg-[#fff] p-6 shadow-lg ${allClasses?.length == 0 && 'h-[200px]'}`}>
       <h2 className="mb-4 text-xl font-semibold text-purple-600 ">Todas Tus Clases</h2>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {(showAllOldClasses
@@ -11,6 +13,7 @@ const AllClasses = ({ allClasses, showAllOldClasses, setShowAllOldClasses }) => 
         ).map((professor) => (
           <ClassCard key={professor.teacher.id} professor={professor} />
         ))}
+         {allClasses?.length == 0 && <div className="gap-4"><div className="w-full">No has acordado una clase aún. ¡Contacta con un profesor en la sección de Result! </div><Button className="bg-purple-600 text-white  mt-2">Result <UserSearch/></Button></div>}
       </div>
       {allClasses && allClasses.length > 3 && (
         <button
--- src/components/dashboard/student/components/UpcomingClasses.jsx
+++ src/components/dashboard/student/components/UpcomingClasses.jsx
@@ -2,12 +2,13 @@ import ClassCard from "./ClassCard";
 
 const UpcomingClasses = ({ classes, showAll, setShowAll }) => {
   return (
-    <div className="rounded-lg bg-[#fff] p-6 shadow-lg">
+    <div className={`rounded-lg bg-[#fff] p-6 shadow-lg ${classes?.length == 0 && 'h-[200px]'}`}>
       <h2 className="mb-4 text-xl font-semibold text-purple-600">Actividades Próximas</h2>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {(showAll ? classes : classes.slice(0, 3)).map((professor) => (
           <ClassCard key={professor.teacher.id} professor={professor} />
         ))}
+        {classes?.length == 0 && <div className="w-[1500px]">No tiene proximas clases en este momento.</div>}
       </div>
       {classes.length > 3 && (
         <button
