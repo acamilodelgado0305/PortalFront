afdd361016d2cd44c185c3d9ff9d9a64ba87d4d8
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-12-16 11:29:53 -0300

    Elimiación de comentarios

--- src/components/WhiteBoardDashBoard/components/WhiteBoard/LeftControlsBar.jsx
+++ src/components/WhiteBoardDashBoard/components/WhiteBoard/LeftControlsBar.jsx
@@ -4,10 +4,10 @@ import { FloatButton } from "antd";
 
 import pencilBlack from "./utils/pencil-black.svg";
 import zoomPlusBlack from "./utils/zoom plus black.svg";
-import zoomMinusBlack from './utils/zoom minus black.svg';
+import zoomMinusBlack from "./utils/zoom minus black.svg";
 import undoBlack from "./utils/undo-black.svg";
 import redoBlack from "./utils/redo-black.svg";
-import trashBlack from './utils/trash-black.svg';
+import trashBlack from "./utils/trash-black.svg";
 import textIconBlack from "./utils/text-icon-black.svg";
 import straightBlack from "./utils/straight-black.svg";
 import squareIconBlack from "./utils/square-icon-black.svg";
@@ -17,11 +17,6 @@ import colorPaletteBlack from "./utils/color-palette-black.svg";
 import circleBlack from "./utils/circle-black.svg";
 import handIconBlack from "./utils/hand-icon-black.svg";
 import eraserBlack from "./utils/eraser-black.svg";
-
-
-
-
-
 
 // GLOBAL
 const emitToSocket = true;
@@ -57,7 +52,11 @@ function LeftControlsBar({ context }) {
     <div className="absolute z-[99] flex h-full flex-col gap-1 bg-[#FFFFFF] py-2 pr-2">
       <PencilEraserToggleButton context={context} style={style} />
       <TextButton context={context} style={style} />
-      <LineWidthPickerButton context={context} style={style} isMovil={isMovil} />
+      <LineWidthPickerButton
+        context={context}
+        style={style}
+        isMovil={isMovil}
+      />
       <ColorOptionButton context={context} style={style} isMovil={isMovil} />
       <UndoRedoButtons context={context} style={style} />
       <CurrentDrawToolPickerButtons context={context} style={style} />
@@ -84,12 +83,13 @@ const Button = ({ children, onClick, style, activeCondition, activeStyle }) => (
 );
 
 const TrashBinClearerButton = ({ context, style }) => (
-  <Button
-    onClick={() => context.clearWhiteBoard(emitToSocket)}
-    style={style}
-  >
+  <Button onClick={() => context.clearWhiteBoard(emitToSocket)} style={style}>
     {/* trashBlack */}
-    <img src={trashBlack}  alt="uploadBlack Icon" style={{ width: "30px", height: "30px" }}/>
+    <img
+      src={trashBlack}
+      alt="uploadBlack Icon"
+      style={{ width: "30px", height: "30px" }}
+    />
   </Button>
 );
 
@@ -100,22 +100,25 @@ const UndoRedoButtons = ({ context, style }) => (
       style={style}
       activeCondition={false}
     >
-    <img src={undoBlack}  alt="uploadBlack Icon" style={{ width: "30px", height: "30px" }}/>
+      <img
+        src={undoBlack}
+        alt="uploadBlack Icon"
+        style={{ width: "30px", height: "30px" }}
+      />
     </Button>
     <Button
       onClick={() => context.redo(emitToSocket)}
       style={style}
       activeCondition={false}
     >
-       <img 
-  src={redoBlack} 
-  alt="redoBlack Icon" 
-  style={{ 
-    width: "30px", 
-    height: "30px", 
-  }} 
-/>
-
+      <img
+        src={redoBlack}
+        alt="redoBlack Icon"
+        style={{
+          width: "30px",
+          height: "30px",
+        }}
+      />
     </Button>
   </>
 );
@@ -126,7 +129,19 @@ const ZoomButton = ({ context, style }) => (
     style={style}
     activeCondition={context.drawingMode === "zoom"}
   >
-    {context.zoomType === "in" ? <img src={zoomPlusBlack}  alt="Zoom plusIcon" style={{ width: "30px", height: "30px" }}/> : <img src={zoomMinusBlack}  alt="Zoom munus Icon" style={{ width: "30px", height: "30px" }}/>}
+    {context.zoomType === "in" ? (
+      <img
+        src={zoomPlusBlack}
+        alt="Zoom plusIcon"
+        style={{ width: "30px", height: "30px" }}
+      />
+    ) : (
+      <img
+        src={zoomMinusBlack}
+        alt="Zoom munus Icon"
+        style={{ width: "30px", height: "30px" }}
+      />
+    )}
   </Button>
 );
 
@@ -138,16 +153,18 @@ const PencilEraserToggleButton = ({ context, style }) => (
         context.updateDrawTool("line", emitToSocket);
       }}
       style={style}
-      activeCondition={context.drawingMode === "draw" && context.currentDrawTool === "line"}
+      activeCondition={
+        context.drawingMode === "draw" && context.currentDrawTool === "line"
+      }
     >
-          <img 
-  src={pencilBlack} 
-  alt="pencilBlack Icon" 
-  style={{ 
-    width: "30px", 
-    height: "30px", 
-  }} 
-/>
+      <img
+        src={pencilBlack}
+        alt="pencilBlack Icon"
+        style={{
+          width: "30px",
+          height: "30px",
+        }}
+      />
     </Button>
     <Button
       onClick={() => {
@@ -156,14 +173,14 @@ const PencilEraserToggleButton = ({ context, style }) => (
       style={style}
       activeCondition={context.drawingMode === "erase"}
     >
-      <img 
-  src={eraserBlack} 
-  alt="eraserBlack Icon" 
-  style={{ 
-    width: "30px", 
-    height: "30px", 
-  }} 
-/>
+      <img
+        src={eraserBlack}
+        alt="eraserBlack Icon"
+        style={{
+          width: "30px",
+          height: "30px",
+        }}
+      />
     </Button>
   </>
 );
@@ -173,26 +190,31 @@ const ColorOptionButton = ({ context, style, isMovil }) => {
   const colors = ["red", "blue", "green", "yellow"];
   return (
     <>
-      {show && colors.map((color, index) => (
-        <FloatButton
-          key={color}
-          onClick={() => context.changeColor(color, emitToSocket)}
-          style={{
-            position: "absolute",
-            height: "43px",
-            width: "43px",
-            left: 50 + index * 50,
-            top: !isMovil ? 205 : 150,
-          }}
-          icon={<FaCircle color={color} />}
-        />
-      ))}
+      {show &&
+        colors.map((color, index) => (
+          <FloatButton
+            key={color}
+            onClick={() => context.changeColor(color, emitToSocket)}
+            style={{
+              position: "absolute",
+              height: "43px",
+              width: "43px",
+              left: 50 + index * 50,
+              top: !isMovil ? 205 : 150,
+            }}
+            icon={<FaCircle color={color} />}
+          />
+        ))}
       <Button
         onClick={() => setShow(!show)}
         style={style}
         activeCondition={false}
       >
-        <img src={colorPaletteBlack}  alt="uploadBlack Icon" style={{ width: "30px", height: "30px" }}/>
+        <img
+          src={colorPaletteBlack}
+          alt="uploadBlack Icon"
+          style={{ width: "30px", height: "30px" }}
+        />
       </Button>
     </>
   );
@@ -209,7 +231,9 @@ const LineWidthPickerButton = ({ context, style, isMovil }) => {
           min="1"
           max="20"
           value={context?.lineWidth || 2}
-          onChange={(e) => context.updateLineWidth(Number(e.target.value), emitToSocket)}
+          onChange={(e) =>
+            context.updateLineWidth(Number(e.target.value), emitToSocket)
+          }
           style={{
             position: "absolute",
             top: !isMovil ? 169 : 129,
@@ -223,7 +247,11 @@ const LineWidthPickerButton = ({ context, style, isMovil }) => {
         style={style}
         activeCondition={false}
       >
-        <img src={sizeIcon} alt="Size Icon" style={{ width: "30px", height: "30px" }} />
+        <img
+          src={sizeIcon}
+          alt="Size Icon"
+          style={{ width: "30px", height: "30px" }}
+        />
       </Button>
     </>
   );
@@ -232,10 +260,46 @@ const LineWidthPickerButton = ({ context, style, isMovil }) => {
 const CurrentDrawToolPickerButtons = ({ context, style }) => {
   // circleBlack
   const tools = [
-    { name: "rectangle", icon: <img src={squareIconBlack} alt="squareIconBlack Icon" style={{ width: "30px", height: "30px" }} /> },
-    { name: "circle", icon: <img src={circleBlack} alt="circleBlack Icon" style={{ width: "30px", height: "30px" }} />  },
-    { name: "straightLine", icon:  <img src={segmentIconBlack} alt="segmentIconBlack Icon" style={{ width: "30px", height: "30px" }} /> },
-    { name: "arrow", icon:  <img src={straightBlack} alt="straightBlack Icon" style={{ width: "30px", height: "30px" }} /> },
+    {
+      name: "rectangle",
+      icon: (
+        <img
+          src={squareIconBlack}
+          alt="squareIconBlack Icon"
+          style={{ width: "30px", height: "30px" }}
+        />
+      ),
+    },
+    {
+      name: "circle",
+      icon: (
+        <img
+          src={circleBlack}
+          alt="circleBlack Icon"
+          style={{ width: "30px", height: "30px" }}
+        />
+      ),
+    },
+    {
+      name: "straightLine",
+      icon: (
+        <img
+          src={segmentIconBlack}
+          alt="segmentIconBlack Icon"
+          style={{ width: "30px", height: "30px" }}
+        />
+      ),
+    },
+    {
+      name: "arrow",
+      icon: (
+        <img
+          src={straightBlack}
+          alt="straightBlack Icon"
+          style={{ width: "30px", height: "30px" }}
+        />
+      ),
+    },
   ];
   return (
     <div className="flex flex-col gap-1">
@@ -244,7 +308,10 @@ const CurrentDrawToolPickerButtons = ({ context, style }) => {
           key={tool.name}
           onClick={() => context.updateDrawTool(tool.name, emitToSocket)}
           style={style}
-          activeCondition={context.currentDrawTool === tool.name && context.drawingMode === "draw"}
+          activeCondition={
+            context.currentDrawTool === tool.name &&
+            context.drawingMode === "draw"
+          }
         >
           {tool.icon}
         </Button>
@@ -259,7 +326,11 @@ const TextButton = ({ context, style }) => (
     style={style}
     activeCondition={context.drawingMode === "text"}
   >
-    <img src={textIconBlack} alt="Text Icon" style={{ width: "30px", height: "30px" }} />
+    <img
+      src={textIconBlack}
+      alt="Text Icon"
+      style={{ width: "30px", height: "30px" }}
+    />
   </Button>
 );
 
@@ -269,7 +340,11 @@ const DruggerButton = ({ context, style }) => (
     style={style}
     activeCondition={context.drawingMode === "hand"}
   >
-    <img src={handIconBlack} alt="Text Icon" style={{ width: "30px", height: "30px" }} /> 
+    <img
+      src={handIconBlack}
+      alt="Text Icon"
+      style={{ width: "30px", height: "30px" }}
+    />
   </Button>
 );
 
