dc6b10b87d6a38f6099ea93eafc59aab0c79cd31
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-12-16 11:49:02 -0300

    Esta pizarra es de prueba no de clase

--- src/components/WhiteBoardDashBoard/components/WhiteBoard/Index.jsx
+++ src/components/WhiteBoardDashBoard/components/WhiteBoard/Index.jsx
@@ -1,30 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAuth } from '../../../../Context/AuthContext.jsx';
+import { pencilCursor, eraserCursor, textCursor, zoomInCursor, zoomOutCursor } from './utils/cursorIcons.js';
+import { Button, message } from 'antd';
+import { Rnd } from "react-rnd";
 import LeftControlsBar from './LeftControlsBar';
 import DrawingCanvas from './DrawingCanvas';
-import { pencilCursor, eraserCursor, textCursor, zoomInCursor, zoomOutCursor } from './utils/cursorIcons.js';
-import SocketListener  from "./SocketListener.jsx"
-import { Button } from 'antd';
-import { Rnd } from "react-rnd";
-function WhiteBoard({socket, context}) {
-  
-  
+import SocketListener from "./SocketListener.jsx";
+
+
+function WhiteBoard({ socket, context }) {
+  const { room } = useParams();
+  const { user } = useAuth();
+  const [messageShown, setMessageShown] = useState(false);
+
+  useEffect(() => {
+    if (user.id === room && !messageShown) {
+      message.warning('Esta pizarra es de prueba no de clase', 3);
+      setMessageShown(true); 
+    }
+  }, [user.id, room, messageShown]);
+
   return (
-<div
-  style={{
-   width:'100%',
-  height:'100%'
-}}
-  
->
-      <BoardHeader
-        context={context}
-      />
-      <SocketListener
-       context={context}
-       socket={socket}
-      />
-      <LeftControlsBar
-       context={context}
-      />
+    <div style={{ width: '100%', height: '100%' }}>
+      <BoardHeader context={context} />
+      <SocketListener context={context} socket={socket} />
+      <LeftControlsBar context={context} />
      
       <Rnd
         default={{
@@ -32,34 +33,29 @@ function WhiteBoard({socket, context}) {
           y: 0
         }}
         dragHandleClassName={"drag-whiteboard-handle"}
-        style={{  cursor:
-          (context.drawingMode === 'draw' && context.currentDrawTool === "line") ? pencilCursor :
-          context.drawingMode === 'hand' ? (context.isgrabbing ? 'grabbing' :'grab') :
-          context.drawingMode === 'erase' ? eraserCursor :
-          context.drawingMode === 'zoom' ? (context.zoomType == 'in' ? zoomInCursor : zoomOutCursor):
-          context.drawingMode === 'text' && textCursor 
-          
+        style={{
+          cursor: context.drawingMode === 'draw' && context.currentDrawTool === "line" ? pencilCursor :
+                  context.drawingMode === 'hand' ? (context.isgrabbing ? 'grabbing' : 'grab') :
+                  context.drawingMode === 'erase' ? eraserCursor :
+                  context.drawingMode === 'zoom' ? (context.zoomType === 'in' ? zoomInCursor : zoomOutCursor) :
+                  context.drawingMode === 'text' && textCursor
         }}
       >
         <DrawingCanvas context={context} />
       </Rnd>
-     {/** */}
     </div>
   );
 }
 
 export default WhiteBoard;
 
-
-
-;
 function BoardHeader({ context, socket }) {
   return (
-    <div className="absolute w-full pl-[59px]">    <Button className="z-[99]" color="default" variant="filled">
- Página {context.currentPage}
-  </Button></div>
+    <div className="absolute w-full pl-[59px]">
+      <Button className="z-[99]" color="default" variant="filled">
+        Página {context.currentPage}
+      </Button>
+    </div>
   );
 }
-
-
 
